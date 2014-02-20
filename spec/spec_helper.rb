f5a821951c46b18603ddc6095a77515116c81336
github username <github email address>
2014-02-20 11:42:23 -0500

    removed filters again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,8 @@
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus, :if
+  config.filter_run :focus
   
-  config.exclusion_filter = {
-    :if => lambda { |what| 
-      case what
-      when :true then example.run
-      end
-    }
-  }
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
